e8b8fc97308a7aae39b930032188f46dc9e1e3cc
Albert Armea <albert@albertarmea.com>
2015-04-04 11:42:48 -0700

    Rename cookies in CancelableTask to IDs

--- CancelableTask.js
+++ CancelableTask.js
@@ -1,27 +1,27 @@
 const TASK_STOPPED = -1;
 
 var CancelableTask = function(callback, timeout) {
-  this.cookie = TASK_STOPPED;
+  this.taskId = TASK_STOPPED;
   this.callback = callback;
   this.timeout = timeout;
 }
 
 CancelableTask.prototype.start = function(callback, timeout) {
-  this.cookie = Math.floor(Math.random() * 2147483648);
+  this.taskId = Math.floor(Math.random() * 2147483648);
 
   var thisTask = this;
-  host.scheduleTask(function(cachedCookie) {
-    if (cachedCookie == thisTask.cookie) {
+  host.scheduleTask(function(cachedId) {
+    if (cachedId == thisTask.taskId) {
       thisTask.callback();
-      thisTask.cookie = TASK_STOPPED;
+      thisTask.taskId = TASK_STOPPED;
     }
-  }, [this.cookie] /*args*/, this.timeout);
+  }, [this.taskId] /*args*/, this.timeout);
 }
 
 CancelableTask.prototype.cancel = function() {
-  this.cookie = TASK_STOPPED;
+  this.taskId = TASK_STOPPED;
 }
 
 CancelableTask.prototype.isActive = function() {
-  return (this.cookie != TASK_STOPPED);
+  return (this.taskId != TASK_STOPPED);
 }
