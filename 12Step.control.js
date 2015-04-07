0ae54e10d5eee3159bf6d0153307291ea216738d
Albert Armea <albert@albertarmea.com>
2015-04-06 23:33:32 -0700

    Change clip key order

--- 12Step.control.js
+++ 12Step.control.js
@@ -12,7 +12,7 @@ const MIDI_NOTE_OFF = 0x80;
 const MIDI_NOTE_ON = 0x90;
 
 // All notes in [C4, C5] except F#, G#, and A#
-const TRACK_CONTROL_NOTES = [48, 49, 50, 51, 52, 53, 55, 57, 59, 60];
+const TRACK_CONTROL_NOTES = [48, 50, 52, 53, 55, 57, 59, 60, 49, 51];
 const STOP_ALL_NOTE = 54; // F# 4
 const SCENE_SWITCH_NOTE = 56; // G# 4
 const PAGE_TURN_NOTE = 58; //  A# 4
